b4dc035f17cfe3c2d96d769fb595c97a5d2c85a2
nadya <nouranadian@gmail.com>
2025-12-07 23:01:56 +0100

    third commit

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  standalone: true,
+  imports: [], // rien à importer !
   templateUrl: './products.html',
-  styleUrl: './products.css',
-  standalone: true
+  styleUrl: './products.css'
 })
-export class Products {
+export class Products implements OnInit {
 
+  products: Array<any> = [];
+
+  ngOnInit(): void {
+    this.products = [
+      { id: 1, name: "computer", price: 3232, selected: true },
+      { id: 2, name: "printer", price: 1232, selected: false },
+      { id: 3, name: "smart Phone", price: 1032, selected: true }
+    ];
+  }
+
+  handelDelete(product: any) {
+    const v = confirm('Êtes-vous sûr de vouloir supprimer ?');
+    if (v) {
+      this.products = this.products.filter(p => p.id !== product.id);
+    }
+  }
 }
